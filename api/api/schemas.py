2be25b697be76da086fae7308a4551445dcf920a
oniko94 <onikolaiev94@outlook.com>
2024-06-13 01:13:32 +0300

    WIP: Step 1. Prepare the bare minimum for introducing Postgres

---
+++ api/api/schemas.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class LogEntryBase(BaseModel):
+    message: str
+    level: str
+
+
+class LogEntry(LogEntryBase):
+    id: int
+    timestamp: datetime
+
+    class Config:
+        from_attributes = True    
